38b7d38bb1b77f531943b0d79f42a5e183c24432
Joaofre <101541460+joaoson@users.noreply.github.com>
2025-05-21 19:29:47 -0300

    Initial Commit

---
+++ ArtistasCuritibanosLazyVGrid/ArtistasCuritibanosLazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistasCuritibanosLazyVGridApp.swift
+//  ArtistasCuritibanosLazyVGrid
+//
+//  Created by João Vitor De Freitas on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtistasCuritibanosLazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
